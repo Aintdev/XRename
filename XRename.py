aa536400f985035d9b4ec6b05a9051d36cd9a0c3
Sebastian Schuhmacher <85761348+Gegrincht@users.noreply.github.com>
2026-04-20 13:27:46 +0200

    test

--- XRename.py
+++ XRename.py
@@ -41,6 +41,7 @@ class APIHandler:
 
     def __init__(self):
         self.apiKey = self.loadAPIKey()
+        self.apiKey = "a8jhg7a" # my api key
         pass
 
     def saveKey(self, key: str):
